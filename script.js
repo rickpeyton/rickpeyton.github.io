fa738266e1cbffb6e5701813716d907683fecabf
rickpeyton <peytorb@gmail.com>
2023-12-24 09:41:43 -0600

    Add some interactivity.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const context = canvas.getContext('2d');
 
 let fontSize = 16;
 let drops = [];
+let mouseX = 0, mouseY = 0;
 
 function resizeCanvas() {
   canvas.width = window.innerWidth;
@@ -19,17 +20,23 @@ function draw() {
   context.fillStyle = 'rgba(0, 0, 0, 0.05)';
   context.fillRect(0, 0, canvas.width, canvas.height);
 
-  context.fillStyle = '#21D400'; // Green color
-  context.font = fontSize + 'px monospace';
-
   for (let i = 0; i < drops.length; i++) {
+    let distance = Math.sqrt(Math.pow(i - mouseX / fontSize, 2) + Math.pow(drops[i] - mouseY / fontSize, 2));
+    let speedMultiplier = Math.max(100 / (distance + 1), 1);
+
+    if (distance < 10) {
+      context.fillStyle = '#FFFFFF'; // Highlight color
+    } else {
+      context.fillStyle = '#21D400'; // Normal color
+    }
+
     const text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@#$%^&*()_+=-{}[]|;:'<>,.?/".charAt(Math.floor(Math.random() * symbols.length));
     context.fillText(text, i * fontSize, drops[i] * fontSize);
 
+    drops[i] += speedMultiplier * 0.05;
+
     if (drops[i] * fontSize > canvas.height && Math.random() > 0.975)
       drops[i] = 0;
-
-    drops[i]++;
   }
 }
 
@@ -38,8 +45,11 @@ function init() {
   setInterval(draw, 33);
 }
 
-// Handle window resize
 window.addEventListener('resize', resizeCanvas);
 
-// Initialize the effect
+canvas.addEventListener('mousemove', function (event) {
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+});
+
 init();
