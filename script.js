bfdd539e8e755a9548f796945eabac17e8bd888d
rickpeyton <peytorb@gmail.com>
2023-12-24 10:29:22 -0600

    Bring back drop effect

--- script.js
+++ script.js
@@ -1,40 +1,8 @@
 const canvas = document.getElementById('matrixCanvas');
 const context = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const fontSize = 12;
+const fontSize = 16;
 const symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@#$%^&*()_+=-{}[]|;:'<>,.?/";
-const columns = canvas.width / fontSize;
-
-const drops = [];
-for (let x = 0; x < columns; x++)
-  drops[x] = 1;
-
-function draw() {
-  context.fillStyle = 'rgba(0, 0, 0, 0.05)';
-  context.fillRect(0, 0, canvas.width, canvas.height);
-
-  context.fillStyle = '#21D400';
-  context.font = fontSize + 'px monospace';
-
-  for (let i = 0; i < drops.length; i++) {
-    const text = symbols.charAt(Math.floor(Math.random() * symbols.length));
-    context.fillText(text, i * fontSize, drops[i] * fontSize);
-
-    if (drops[i] * fontSize > canvas.height)
-      drops[i] = 0;
-
-    drops[i]++;
-  }
-}
-
-
-function init() {
-  resizeCanvas();
-  setInterval(draw, 50);
-}
 
 function resizeCanvas() {
   const pixelRatio = window.devicePixelRatio || 1;
@@ -45,14 +13,40 @@ function resizeCanvas() {
 
   context.scale(pixelRatio, pixelRatio);
 
-  // Safeguard to ensure columns is a valid number
   const columns = Math.floor(canvas.width / fontSize);
-  if (columns > 0 && Number.isFinite(columns)) {
-    drops.length = columns;
-    for (let x = 0; x < columns; x++)
-      drops[x] = 1;
+  const prevDropsLength = drops.length;
+
+  // Resize drops array while preserving existing values
+  drops.length = columns;
+  for (let x = prevDropsLength; x < columns; x++)
+    drops[x] = 1;
+}
+
+function draw() {
+  context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = '#21D400'; // Green color
+  context.font = fontSize + 'px monospace';
+
+  for (let i = 0; i < drops.length; i++) {
+    const text = symbols.charAt(Math.floor(Math.random() * symbols.length));
+    context.fillText(text, i * fontSize, drops[i] * fontSize);
+
+    if (drops[i] * fontSize > canvas.height && Math.random() > 0.975)
+      drops[i] = 0;
+
+    drops[i]++;
   }
 }
+
+function init() {
+  resizeCanvas();
+  setInterval(draw, 33);
+}
+
+// Initialize drops array
+const drops = Array(Math.floor(canvas.width / fontSize)).fill(1);
 
 window.addEventListener('resize', resizeCanvas);
 
