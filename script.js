d8ef735f9bdd3c069a2857e55ce643b5cca59da7
rickpeyton <peytorb@gmail.com>
2023-12-24 10:09:16 -0600

    Go back to the original implementation.

--- script.js
+++ script.js
@@ -1,44 +1,32 @@
 const canvas = document.getElementById('matrixCanvas');
 const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const fontSize = 16;
 const symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@#$%^&*()_+=-{}[]|;:'<>,.?/";
+const columns = canvas.width / fontSize;
 
-
-let fontSize = 16;
-let drops = [];
-let mouseX = 0, mouseY = 0;
-
-function resizeCanvas() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  const columns = canvas.width / fontSize;
-  drops = [];
-
-  for (let x = 0; x < columns; x++)
-    drops[x] = 1;
-}
+const drops = [];
+for (let x = 0; x < columns; x++)
+  drops[x] = 1;
 
 function draw() {
   context.fillStyle = 'rgba(0, 0, 0, 0.05)';
   context.fillRect(0, 0, canvas.width, canvas.height);
 
+  context.fillStyle = '#21D400'; // Green color
+  context.font = fontSize + 'px monospace';
+
   for (let i = 0; i < drops.length; i++) {
-    let distance = Math.sqrt(Math.pow(i - mouseX / fontSize, 2) + Math.pow(drops[i] - mouseY / fontSize, 2));
-    let speedMultiplier = Math.max(100 / (distance + 1), 1);
-
-    if (distance < 10) {
-      context.fillStyle = '#FFFFFF'; // Highlight color
-    } else {
-      context.fillStyle = '#21D400'; // Normal color
-    }
-
     const text = symbols.charAt(Math.floor(Math.random() * symbols.length));
     context.fillText(text, i * fontSize, drops[i] * fontSize);
 
-    drops[i] += speedMultiplier * 0.05;
-
     if (drops[i] * fontSize > canvas.height && Math.random() > 0.975)
       drops[i] = 0;
+
+    drops[i]++;
   }
 }
 
@@ -47,11 +35,14 @@ function init() {
   setInterval(draw, 33);
 }
 
-window.addEventListener('resize', resizeCanvas);
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  drops.length = canvas.width / fontSize;
+  for (let x = 0; x < drops.length; x++)
+    drops[x] = 1;
+}
 
-canvas.addEventListener('mousemove', function (event) {
-  mouseX = event.clientX;
-  mouseY = event.clientY;
-});
+window.addEventListener('resize', resizeCanvas);
 
 init();
