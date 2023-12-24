8879bdfcee276ea6768c8afb7b9367317116505c
rickpeyton <peytorb@gmail.com>
2023-12-24 10:17:58 -0600

    simplify for mobile

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const fontSize = 16;
+const fontSize = 12;
 const symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@#$%^&*()_+=-{}[]|;:'<>,.?/";
 const columns = canvas.width / fontSize;
 
@@ -16,30 +16,38 @@ function draw() {
   context.fillStyle = 'rgba(0, 0, 0, 0.05)';
   context.fillRect(0, 0, canvas.width, canvas.height);
 
-  context.fillStyle = '#21D400'; // Green color
+  context.fillStyle = '#21D400';
   context.font = fontSize + 'px monospace';
 
   for (let i = 0; i < drops.length; i++) {
     const text = symbols.charAt(Math.floor(Math.random() * symbols.length));
     context.fillText(text, i * fontSize, drops[i] * fontSize);
 
-    if (drops[i] * fontSize > canvas.height && Math.random() > 0.975)
+    if (drops[i] * fontSize > canvas.height)
       drops[i] = 0;
 
     drops[i]++;
   }
 }
 
+
 function init() {
   resizeCanvas();
-  setInterval(draw, 33);
+  setInterval(draw, 50);
 }
 
 function resizeCanvas() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  drops.length = canvas.width / fontSize;
-  for (let x = 0; x < drops.length; x++)
+  const pixelRatio = window.devicePixelRatio || 1;
+  canvas.width = window.innerWidth * pixelRatio;
+  canvas.height = window.innerHeight * pixelRatio;
+  canvas.style.width = window.innerWidth + 'px';
+  canvas.style.height = window.innerHeight + 'px';
+
+  context.scale(pixelRatio, pixelRatio);
+
+  const columns = canvas.width / fontSize;
+  drops.length = columns;
+  for (let x = 0; x < columns; x++)
     drops[x] = 1;
 }
 
